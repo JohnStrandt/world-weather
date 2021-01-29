e286d500e647ab03bd2f508125514769a27cd42d
JohnStrandt <jstrandt@hotmail.com>
2021-01-29 14:17:47 -0600

    git ignore

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 const proxy = "https://cors-anywhere.herokuapp.com/";
-const key = "9a909d04d1e5f28bc051154f7307bdb7";
+const key = process.env.REACT_APP_WEATHER_KEY;
 const base_url = `api.openweathermap.org/data/2.5/`;
 
+export const searchURL = (city) =>
+  `${proxy}${base_url}weather?q=${city}&units=imperial&appid=${key}`;
 
-export const searchURL = (city) =>`${proxy}${base_url}weather?q=${city}&units=imperial&appid=${key}`;
-
-export const forecastURL = (lat, lon) => `${proxy}${base_url}onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
+export const forecastURL = (lat, lon) =>
+  `${proxy}${base_url}onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
