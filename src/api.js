974e01738330282a815035e345c16020f500e886
JohnStrandt <jstrandt@hotmail.com>
2024-11-05 20:21:08 -0600

    Refactor for new api endpoint

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const key = process.env.REACT_APP_WEATHER_KEY;
 const base_url = `https://api.openweathermap.org/data/2.5/`;
+const one_call_url = `https://api.openweathermap.org/data/3.0/`;
 
 export const gpsURL = (lat, lon) =>
   `${base_url}weather?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
@@ -8,4 +9,4 @@ export const searchURL = (city) =>
   `${base_url}weather?q=${city}&units=imperial&appid=${key}`;
 
 export const forecastURL = (lat, lon) =>
-  `${base_url}onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
+  `${one_call_url}onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
